88317707d708d957f9587fe917730d33146c1919
skapy-skar <skapy.skar@gmail.com>
2025-04-03 02:56:21 +0530

    pkmnmastertraining.py

--- pkmnmastertraining.py
+++ pkmnmastertraining.py
@@ -3,12 +3,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 def train_pkmn(df):
-    dropclmn = ["#", "Name"]
-    dropclmn = [c for c in dropclmn if c in df.columns]
-    df = df.drop(columns=dropclmn)
-    x = df.drop(columns=["Mega_Evolution"])
-    y = df["Mega_Evolution"]
-    xtr, xte, ytr, yte = train_test_split(x, y, test_size=0.2, random_state=42)
-    trainer = LogisticRegression(max_iter=2000)
-    trainer.fit(xtr, ytr)
-    return trainer, xte, yte
+    drop=["#","Name"]
+    df=df.drop(columns=drop)
+    x=df.drop(columns=["Mega_Evolution"])
+    y=df["Mega_Evolution"]
+    print(y.value_counts())
+    xtr,xte,ytr,yte=train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
+    trainer=LogisticRegression(max_iter=2000)
+    trainer.fit(xtr,ytr)
+    return trainer,xte,yte
